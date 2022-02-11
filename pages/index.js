b8cabad46c85e7fe2fb9423f60bda7e56205c52a
David McCullough <davidmcculloughw@gmail.com>
2022-02-11 09:41:06 -0700

    styling

--- pages/index.js
+++ pages/index.js
@@ -19,54 +19,32 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>{p.price}</h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h1 className={styles.title}>{"📦 Gachapon Packer"}</h1>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href={"/shirts"} className={styles.card}>
+            <h2>Stock &rarr;</h2>
+            <p>View inventory to be packed</p>
           </a>
 
-          <a onClick={() => Run()} className={styles.card}>
-            <h2>Run &rarr;</h2>
-            <p>Generate some boxes</p>
+          <a href={"/api/hello"} className={styles.card}>
+            <h2>Pack &rarr;</h2>
+            <p>Generate some Gachapon packing lists</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div />
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href={
+            "https://docs.google.com/spreadsheets/d/1EpAYJPnfnADNrEEPToE3pNGiZR4mREX-u0NIM4gu9TA"
+          }
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Edit Data Source &#8599;
         </a>
       </footer>
     </div>
